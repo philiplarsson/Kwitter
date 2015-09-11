57ea14dc225714ceb86ca42c16f931e67dff3b5e
Philip Larsson <elt11pla@student.lu.se>
2015-09-11 13:49:56 +0200

    Add form to view and styling in css file

--- views/login.view.php
+++ views/login.view.php
@@ -1,1 +1,20 @@
 <h3>Login Page</h3>
+<form action="" method="post">
+  
+  <ul>
+    <li>
+      <label for="username">Username: </label>
+      <input name="email" type="text"/>
+    </li>
+    <li>
+      <label for="email">Email: </label>
+      <input name="email" type="text"/>
+    </li>
+    
+    <li>
+      <input class="login_button" name="" type="submit" value="Log In"/>
+      <input class="login_button" name="" type="reset" value="Reset"/>
+    </li>
+  </ul>
+  
+</form>
