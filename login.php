4827fd2a2278fc13de45b0754633daa682c7b084
Philip Larsson <elt11pla@student.lu.se>
2015-09-11 14:11:51 +0200

    Add styling for status message

--- login.php
+++ login.php
@@ -8,10 +8,10 @@ if ( $_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
     $username = $_POST["username"];
     if ( empty($password) || empty($username)) {
-        $status = "Missing email or username";
+        $status = "Missing email or username.";
     } else {
         // log in user
-        $status = "Thanks for logging in with $username and $password";
+        $status = "Thanks for logging in with $username and $password.";
     }
 }
 
