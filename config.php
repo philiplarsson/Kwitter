4e36891fc6c7d381d4b146e1535e32f2c3731a3e
Philip Larsson <elt11pla@student.lu.se>
2015-09-11 12:56:25 +0200

    Fix typo

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
  * Config file for site configuration.
  *
  * This file handles the config variables through an array.
- * User the $config array to get the config settings.
+ * Use the $config array to get the config settings.
 */
 
 $config = array(
