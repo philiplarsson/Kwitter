36ff2d522a0273f156d1d28d14f1ede311c598e0
Philip Larsson <elt11pla@student.lu.se>
2015-09-15 20:16:10 +0200

    Add a register form to register page

--- views/register.view.php
+++ views/register.view.php
@@ -1,1 +1,28 @@
 <h1>This is a registration site.</h1>
+
+<form action="" method="post">
+<ul>
+  
+  <li>
+    <label for="username">Username: </label>
+    <input name="username" type="text">
+  </li>
+  <li>
+    <label for="name">Name: </label>
+    <input name="name" type="text">
+  </li>
+  <li>
+    <label for="email">Email: </label>
+    <input name="email" type="text"/>
+  </li>
+  <li>
+    <label for="password">Password:</label>
+    <input name="password" type="password"/>
+  </li>
+
+  <li>
+    <input name="" type="submit" value="Register"/>
+    <input name="" type="reset" value="Reset"/>
+  </li
+</ul>
+</form>
