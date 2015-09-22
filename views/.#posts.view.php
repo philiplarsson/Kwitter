deba068f1a7ecfb659a78fcea546e73747c16885
Philip Larsson <elt11pla@student.lu.se>
2015-09-22 16:51:36 +0200

    Delete emacs auto save files

--- views/.#posts.view.php
+++
@@ -1,1 +0,0 @@
-philiplarson@Philips-MacBook-Pro.local.10912
--- views/posts.view.php
+++ views/posts.view.php
@@ -1,6 +1,6 @@
 <h1>Welcome to Kwitter</h1>
 
-<h2>Posts</h2>
+<h2>Posts / Kweets</h2>
 <p>
   Ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor. Ut ornare lectus sit amet!
 </p>
