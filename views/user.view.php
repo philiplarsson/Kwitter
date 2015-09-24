b051c2663c71eaf1698681380a71baaa23278571
Philip Larsson <elt11pla@student.lu.se>
2015-09-24 03:43:35 +0200

    Add kwitter text area on user page

--- views/user.view.php
+++ views/user.view.php
@@ -1,19 +1,26 @@
-
-
 <h1><?= $name; ?></h1>
 
-<div class="user-information">
-    <img src=" <?= getGravatarImage($email); ?>" />
-    
-    <ul>
-        <li><p><?= $email; ?></p></li>
-        <li><p><?= $username; ?></p></li>    
-    </ul>
+<div class="user-page-container">
+    <div class="user-information">
+        <img src=" <?= getGravatarImage($email); ?>" />
+        
+        <ul>
+            <li><p><?= $email; ?></p></li>
+            <li><p><?= $username; ?></p></li>    
+        </ul>
+    </div>
+
+<div class="new-kweet">
+    <form action="" method="post">
+        <p>Enter your new kweet below:</p>
+        <textarea placeholder="Enter something witty here!" type="text" name="kweet" class="new-kweet-textarea"></textarea>
+        <br>
+        <input class="styled-button" type="submit" value="Kweet!"/>
+    </form>
 </div>
-</br>
+
+
+</div>
 <div class="user-menu">
     <a class="styled-button" href='logout.php'>logout</a>    
 </div>
-
-
-
