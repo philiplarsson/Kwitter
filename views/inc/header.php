b4e7f9996f134b42503febe1cdc6d5b29a0a107c
Philip Larsson <elt11pla@student.lu.se>
2015-10-10 11:33:33 +0200

    Hide some menu options when user is logged in

--- views/inc/header.php
+++ views/inc/header.php
@@ -1,26 +1,32 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8"/>
-    <title><?= get_title($my_title); ?></title>
-    <link href="../../css/normalize.css" rel="stylesheet" type="text/css"/>
-    <link href="../../css/style.css" rel="stylesheet" type="text/css"/>
-    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
-  </head>
-  <body>
-      <header>
-          <div class="logo">Kwitter</div>
-          <nav>
-              <ul>
-                  <li><a href="index.php">Home</a></li>
-                  <li><a href="posts.php">Kweets</a></li>
-                  <li><a href="login.php">Login</a></li>
-                  <li><a href="register.php">Register</a></li>
-                  <?php if( isset( $_SESSION["username"] )) : ?>
-                      <li><a href="user.php">User Page</a></li>
-                  <?php endif; ?>
-              </ul>
-          </nav>
-      </header>
+    <head>
+        <meta charset="UTF-8"/>
+        <title><?= get_title($my_title); ?></title>
+        <link href="../../css/normalize.css" rel="stylesheet" type="text/css"/>
+        <link href="../../css/style.css" rel="stylesheet" type="text/css"/>
+        <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header>
+            <div class="logo">Kwitter</div>
+            <nav>
+                <?php if ( isset($_SESSION["username"]) ): ?>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="posts.php">Kweets</a></li>
+                        <li><a href="user.php">User Page</a></li>
+                    </ul>
+                <?php else: ?>
+                    <!-- User not logged in -->
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="posts.php">Kweets</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="register.php">Register</a></li>
+                <?php endif; ?>
+                    </ul>
+            </nav>
+        </header>
